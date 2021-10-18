a621975683b2484c4965ff155e13166881f7a5cc
Dmitrosio <90358756+Dmitrosio@users.noreply.github.com>
2021-10-18 14:58:49 -0700

    Add files via upload

--- countdown.js
+++ countdown.js
@@ -1,31 +1,24 @@
 // Dmitrii Orlov
 let currTime = 50;
-setTimeout(CountDown, 0000)
-setTimeout(CountDown, 5000)
-setTimeout(CountDown, 10000)
-setTimeout(CountDown, 15000)
-setTimeout(CountDown, 20000)
-setTimeout(CountDown, 25000)
-setTimeout(CountDown, 30000)
-setTimeout(CountDown, 35000)
-setTimeout(CountDown, 40000)
-setTimeout(CountDown, 45000)
-setTimeout(CountDown, 50000)
-
-function CountDown(){
-        if(currTime == 0){
-                alert("Blastoff");
-                document.getElementById("counter").innerHTML = ("Countdown: "+ currTime);
-                document.getElementById("counter").innerHTML = ("Gone to space, will be back soon!");
-        }
-        else{
-                document.getElementById("counter").innerHTML = ("Countdown: "+ currTime);
-        }
-        currTime -= 5;
-        if(currTime <= 25){
-                document.getElementById("counter").innerHTML = ("Warning Less than ½ way to launch, time left: " + currTime);
-        }
+for(i = 0; i <=10; i++){
+        setTimeout(function(){
+                
+                        if(currTime == 0){
+                                alert("Blastoff");
+                                document.getElementById("counter").innerHTML = ("Countdown: "+ currTime);
+                                document.getElementById("counter").innerHTML = ("Gone to space, will be back soon!");
+                        } 
+                        else if(currTime <= 25){
+                                document.getElementById("counter").innerHTML = ("Warning Less than ½ way to launch, time left: " + currTime);
+                        }
+                        else{
+                                document.getElementById("counter").innerHTML = ("Countdown: "+ currTime);
+                        }
+                        currTime -= 5;    
+                
+        },i * 5000);
 }
+
 
 function changeColor(newColor){
         let elem = document.getElementById("counter");
